9a8591511e8b96cdaa64eb9ddfa7f0e305f625c9
jkarjun <jkarjunsingh@gmail.com>
2023-10-11 17:34:55 +0530

    this is the first commit 11 Oct 5:34pm

---
+++ app/src/main/java/com/example/mgithubexample/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.mgithubexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mgithubexample.ui.theme.MGitHubExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MGitHubExampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background,
+                ) {
+                    Column(modifier = Modifier.fillMaxWidth(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+
+                    ) {
+                        Greeting("Satwinder")
+                        var list = listOf<String>("jatin","satwinder","Raman","Sahil","Hrmam","Abhishek")
+                        MyLazyClm(list)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!", textAlign = TextAlign.Center)
+}
+
+@Composable
+fun MyLazyClm(items: List<String>) {
+    LazyColumn(modifier = Modifier.fillMaxWidth(),  verticalArrangement = Arrangement.Center, contentPadding = PaddingValues(10.dp))
+    {
+        items(items){
+            item ->  
+            Text(text = item, fontSize = 18.sp)
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MGitHubExampleTheme {
+        Greeting("Android")
+    }
+}
